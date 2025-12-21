d39ceeb863443e0852d9ae977081292a051fa913
Nathan-Houel <houel.nathan.18@gmail.com>
2025-12-21 21:59:06 +0100

    Last version

--- src/model.py
+++ src/model.py
@@ -1,4 +1,4 @@
 import segmentation_models_pytorch as smp
 
-def get_model(self):
+def get_model():
     return smp.Unet(encoder_name="resnet34", encoder_weights="imagenet", in_channels=3, classes=1)
